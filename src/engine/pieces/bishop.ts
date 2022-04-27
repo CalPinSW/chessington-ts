7c7bd7413cb9d986790e317cf2de908f64fef67d
Calum Pinder <Calum.Pinder@softwire.com>
2022-04-27 10:03:21 +0100

    Basic Bishop moves added.

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import MovesAvailable from "./movesAvailable";
 
 export default class Bishop extends Piece {
     constructor(player: Player) {
@@ -8,6 +9,18 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentPosition = board.findPiece(this);
+        let movesAvailable = new MovesAvailable;
+        for (let rowIndex = 0; rowIndex <= 7; rowIndex++){
+            for (let colIndex = 0; colIndex <= 7; colIndex++){
+                if (rowIndex !== currentPosition.row || colIndex !== currentPosition.col){
+                    if (Math.abs(rowIndex - currentPosition.row) === Math.abs(colIndex - currentPosition.col)){
+                        movesAvailable.add(rowIndex, colIndex);
+                    }
+                }
+
+            }
+        }
+        return  movesAvailable.list;
     }
 }
