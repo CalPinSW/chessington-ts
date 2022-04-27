c4e89f6d2ea15313f8d31869b56b03e1dbc2cf1f
Calum Pinder <Calum.Pinder@softwire.com>
2022-04-27 10:16:34 +0100

    Basic Knight moves added.

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,24 @@
 import Piece from './piece';
 import Board from "../board";
 import Player from "../player";
+import MovesAvailable from "./movesAvailable";
 
 export default class Knight extends Piece {
     constructor(player: Player) {
         super(player);
     }
 
-    getAvailableMoves(board: Board) {
-        return new Array(0);
+    getAvailableMoves(board: Board){
+        let currentPosition = board.findPiece(this);
+        let movesAvailable = new MovesAvailable;
+        for (let rowIndex = 0; rowIndex <= 7; rowIndex++) {
+            for (let colIndex = 0; colIndex <= 7; colIndex++) {
+                if (rowIndex !== currentPosition.row && colIndex !== currentPosition.col && Math.abs(rowIndex - currentPosition.row) + Math.abs(colIndex - currentPosition.col) === 3) {
+                    movesAvailable.add(rowIndex, colIndex);
+                }
+            }
+        }
+        return movesAvailable.list;
     }
 }
+
